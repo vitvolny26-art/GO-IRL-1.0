0d72ad29f1e6a4f0323b3e5198020b63e5cb38ec
vitvolny26-art <vitvolny26@gmail.com>
2026-07-10 06:15:30 +0200

    fix: load bottom nav swipe handler

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,13 @@ import "./coach-panel.css";
 import "./weather-ui-fixes.css";
 import "./generic-sheet-fixes.css";
 import { enableCardActionsEnhancer } from "./card-actions-enhancer";
+import { enableBottomNavSwipe } from "./bottom-nav-swipe";
 
 const App = lazy(() => import("./App"));
 const queryClient = new QueryClient();
 
 enableCardActionsEnhancer();
+enableBottomNavSwipe();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
