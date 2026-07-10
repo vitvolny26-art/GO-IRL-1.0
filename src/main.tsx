9782bf9f946901dfa7b6d0e4adaa0ef0774dfe2a
vitvolny26-art <vitvolny26@gmail.com>
2026-07-10 15:59:17 +0200

    style: load final compact card overrides

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import "./weather-ui-fixes.css";
 import "./generic-sheet-fixes.css";
 import "./sport-avatar-fixes.css";
 import "./compact-sport-card.css";
+import "./compact-sport-card-final.css";
 
 const App = lazy(() => import("./App"));
 const queryClient = new QueryClient();
