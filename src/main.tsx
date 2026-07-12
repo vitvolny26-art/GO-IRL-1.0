5e77735941eeb9b46d0ffeea9949d414f395eab6
vitvolny26-art <vitvolny26@gmail.com>
2026-07-12 14:15:51 +0200

    feat: enable avatar photo cropping

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { lazy, StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { enableAvatarCropper } from "./avatarCropper";
 import { enableBottomNavSwipe } from "./bottom-nav-swipe";
 import { enableCardTextCleanup } from "./cardText";
 import { enableFullCreateTaxonomy } from "./fullCreateTaxonomy";
@@ -14,6 +15,7 @@ import "./compact-sport-card-final.css";
 import "./all-event-card-template.css";
 import "./unified-card-actions.css";
 import "./card-share-action.css";
+import "./avatar-cropper.css";
 
 const App = lazy(() => import("./App"));
 const queryClient = new QueryClient();
@@ -21,6 +23,7 @@ const queryClient = new QueryClient();
 enableBottomNavSwipe();
 enableCardTextCleanup();
 enableFullCreateTaxonomy();
+enableAvatarCropper();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
