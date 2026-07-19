9e6b3e72e7e835b45fec3e3bec2a037ba8e5d6a4
vitvolny26-art <vitvolny26@gmail.com>
2026-07-19 05:45:26 +0200

    FIX1003-fix: accept persisted event ids in invitation links

--- src/invitationLink.ts
+++ src/invitationLink.ts
@@ -1,4 +1,4 @@
-const eventUuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+const eventUuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
 export const isValidInvitationEventId = (value: string) => eventUuidPattern.test(value.trim());
 
