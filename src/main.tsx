97a07df583ef9cba08961ddfa0fa9cdac93f1b50
vitvolny26-art <vitvolny26@gmail.com>
2026-07-24 16:06:55 +0200

    fix: enable Mapy.com runtime map actions

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { enableFullCreateTaxonomy } from "./fullCreateTaxonomy";
 import { enableParticipantJoinNotifications } from "./participantNotifications";
+import { enableMapyRuntimeActions } from "./mapyRuntimeActions";
 import { OrganizerProfilePortal } from "./components/OrganizerProfilePortal";
 import { OrganizerEventDetailsPortal } from "./components/OrganizerEventDetailsPortal";
 import { EventLocationPickerPortal } from "./components/EventLocationPickerPortal";
@@ -33,6 +34,7 @@ const queryClient = new QueryClient();
 
 enableFullCreateTaxonomy();
 enableParticipantJoinNotifications();
+enableMapyRuntimeActions();
 
 if (import.meta.env.PROD && "serviceWorker" in navigator) {
   window.addEventListener("load", () => {
