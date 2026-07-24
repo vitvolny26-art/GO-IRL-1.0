86cb07393194bc4477a15c34260910254c2904e7
vitvolny26-art <vitvolny26@gmail.com>
2026-07-24 19:37:13 +0200

    style: load event details share layout

--- src/main.tsx
+++ src/main.tsx
@@ -31,6 +31,7 @@ import "./organizer-event-details.css";
 import "./event-location-picker.css";
 import "./event-details-v2.css";
 import "./event-details-v2-polish.css";
+import "./event-details-share-v2.css";
 
 const App = lazy(() => import("./App"));
 const queryClient = new QueryClient();
